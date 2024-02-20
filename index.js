cb916bcc0b67957edbd9f669c7285694c66aece1
AdrianVilla1504 <adriancvilla@gmail.com>
2024-02-20 07:56:23 -0500

    feature: params routes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.get('/', (req, res)=>{
   res.send('Hi express server');
@@ -12,11 +12,36 @@ app.get('/v1/healtcheck', (req, res) => {
 });
 
 app.get('/v1/products', (req, res) => {
+  res.json([
+    {
+      name: 'Product 1',
+      price: 1000
+    },
+    {
+      name: 'Product 2',
+      price: 2000
+    }
+  ])
+});
+
+app.get('/v1/products/:id', (req, res) => {
+  const { id } = req.params;
   res.json({
-    name: 'Product 1',
-    price: 1000
+    id,
+    name: 'Product 2',
+    price: 2000
   })
 });
+
+app.get('/v1/categories/:categoryId/products/:productId', (req, res)=> {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId,
+  })
+});
+
+
 
 app.listen(port, () => {
   console.log('My port ' + port)
