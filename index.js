24baf85975f2152ee12e169f979a9a5bf2d37515
AdrianVilla1504 <adriancvilla@gmail.com>
2024-02-21 10:21:04 -0500

    feature: add error middleware

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const app = express();
 const port = process.env.PORT || 3000;
 const routerApi = require('./routes/v1/')
 
+const { logErrors, errorHandler } = require('./middlewares/error.handler' );
+
 app.use(express.json());
+
 
 app.listen(port, () => {
   console.log('App running on port ' + port)
 });
 
 routerApi(app);
+
+app.use(logErrors);
+app.use(errorHandler);
