f78683159e29ab4c6da3a1f9551af8bba1fd5a43
AdrianVilla1504 <adriancvilla@gmail.com>
2024-02-20 21:32:00 -0500

    feature: implement product services layer

--- routes/v1/products.router.js
+++ routes/v1/products.router.js
@@ -1,18 +1,12 @@
 const express = require('express');
+const productsService = require('../../services/product.service')
+
 const router = express.Router();
-const { faker } = require('@faker-js/faker');
+
+const service = new productsService();
 
 router.get('/', (req, res)=>{
-  const products  = [];
-  const { size }  = req.query;
-  const limit = size || 10;
-  for (let index = 0; index < limit; index ++){
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.url(),
-    })
-  }
+  const products  = service.find();
   res.json(products)
 });
 
@@ -22,11 +16,8 @@ router.get('/filter', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-  res.json({
-    id,
-    name: 'Product 2',
-    price: 2000
-  })
+  const product  = service.findOne(id);
+  res.json(product)
 });
 
 router.get('/:categoryId/products/:productId', (req, res)=> {
@@ -39,10 +30,19 @@ router.get('/:categoryId/products/:productId', (req, res)=> {
 
 router.post('/', (req, res) => {
   const body = req.body;
-  res.json({
+  res.status(201).json({
     message: 'created',
     data: body
   });
+})
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: 'success',
+    data: body
+  })
 })
 
 router.patch('/:id', (req, res) => {
