fc02c6864876e3b9612b3f7ecd8a3aaaf796c4aa
AdrianVilla1504 <adriancvilla@gmail.com>
2024-02-21 22:03:23 -0500

    feature: create closure middleware for schema validation

--- routes/v1/products.router.js
+++ routes/v1/products.router.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const productsService = require('../../services/product.service')
+const productsService = require('../../services/product.service');
+const validatorHandler = require('../../middlewares/validator.handler');
+const { createProductSchema, updateProductSchema, getProductSchema } = require('../../schemas/product.schema');
 
 const router = express.Router();
 
@@ -14,14 +16,16 @@ router.get('/filter', async (req, res) => {
   res.send('Yo soy un filter');
 })
 
-router.get('/:id', async (req, res, next) => {
-  try{
-    const { id } = req.params;
-    const product = await service.findOne(id);
-    res.json(product);
-  }catch(error){
-    next(error);
-  }
+router.get('/:id',
+  validatorHandler(getProductSchema, 'params'),
+  async (req, res, next) => {
+    try{
+      const { id } = req.params;
+      const product = await service.findOne(id);
+      res.json(product);
+    }catch(error){
+      next(error);
+    }
 
 });
 
@@ -33,24 +37,29 @@ router.get('/:categoryId/products/:productId', async (req, res)=> {
   })
 });
 
-router.post('/', async (req, res) => {
-  const body = req.body;
-  const newProduct = await service.create(body);
-  res.status(201).json(newProduct);
-})
-
-router.patch('/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
+router.post('/',
+  validatorHandler(createProductSchema, 'body'),
+  async (req, res) => {
     const body = req.body;
-    const product = await service.update(id, body);
-    res.json(product);
-  } catch (error) {
-    res.status(404).json({
-      message: error.message
-    })
-  }
-})
+    const newProduct = await service.create(body);
+    res.status(201).json(newProduct);
+  })
+
+router.patch('/:id',
+  validatorHandler(createProductSchema, 'params'),
+  validatorHandler(updateProductSchema, 'body'),
+  async (req, res) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const product = await service.update(id, body);
+      res.json(product);
+    } catch (error) {
+      res.status(404).json({
+        message: error.message
+      })
+    }
+  })
 
 
 router.put('/:id', async (req, res) => {
