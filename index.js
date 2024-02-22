75298d3dd77ab3bbea919feee6c81c3f831c53fc
AdrianVilla1504 <adriancvilla@gmail.com>
2024-02-21 22:15:25 -0500

    feature: add cors package to allow crossed origin

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 const routerApi = require('./routes/v1/')
@@ -6,7 +7,17 @@ const routerApi = require('./routes/v1/')
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler' );
 
 app.use(express.json());
-
+const whitelist =  ['http://localhost:8080', 'https://myapp.co']
+const options = {
+  origin: (origin, callback)  => {
+    if(whitelist.includes(origin)){
+      callback(null, true)
+    } else {
+      callback(new Error('no permitido'));
+    }
+  }
+}
+app.use(cors(whitelist));
 routerApi(app);
 
 app.use(logErrors);
