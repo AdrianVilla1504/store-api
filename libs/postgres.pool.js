ee293de025874479356f75b8ae5b6e1a5d6cc371
AdrianVilla1504 <adriancvilla@gmail.com>
2024-02-23 14:28:41 -0500

    feature: add config env variables to postgres pool

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -1,12 +1,10 @@
 const { Pool } = require('pg');
+const  config = require('./../config/config');
 
-  const pool = new Pool({
-    host: 'localhost',
-    port: 5432,
-    user: 'adrian',
-    password: '1143412278',
-    database: 'store-api'
-  });
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `postgres:${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
+const pool = new Pool({ connectionString: URI });
 
 module.exports = pool;
